40016e7089bd9189c0ea9641b25e7a6dbbb60342
Cybernetics Core <corecybernetics354@gmail.com>
2021-01-22 01:21:09 +0700

    Adding splash

--- lib/textify_core.dart
+++ lib/textify_core.dart
@@ -1,1 +1,10 @@
 library textify_core;
+
+import 'dart:async';
+
+import 'package:path_provider/path_provider.dart';
+import 'package:sembast/sembast.dart';
+import 'package:path/path.dart';
+import 'package:sembast/sembast_io.dart';
+
+part 'functions/database.dart';
